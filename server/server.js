afab7889427da2d7c761fc88e3961bbaecfca84e
Taranjeet Singh <taranjeets120@gmail.com>
2025-11-12 15:46:47 +0530

    created backend auth api's and routes with middlewares

--- server/server.js
+++ server/server.js
@@ -1,27 +1,20 @@
 import express from 'express'
-import {chats} from './data/data.js'
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
 import connectDB from './config/db.js';
 import colors from 'colors';
+import userRoutes from './routes/userRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.get('/', (req, res) => {
-    res.send('Hello from server');
-});
 
-app.get('/api/chat', (req, res) => {
-    res.send(chats)
-})
-app.get('/api/chat/:id', (req, res) => {
-    // console.log(req.params.id);
-    const singleChat = chats.find((c) => c._id === req.params.id);
-    res.send(singleChat);
-})
+app.use(express.json()); // to accept JSON data
+app.use('/api/user', userRoutes);
 
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}` .yellow.bold);
